217d83ee80b9beef55e6ef75920d1c87a4f3cc98
criscgv7 <cris.cgv7@gmail.com>
2023-04-11 00:39:40 +0200

    TomaDayana

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -15,11 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private static final String[] SECURED_URLs = {"/posts/**"};
+    private static final String[] SECURED_URLs = {"/users/**"};
 
-    private static final String[] UN_SECURED_URLs = {
-            "/users/**"
-    };    
+    private static final String[] UN_SECURED_URLs = {"/posts/**"};    
 
 
     @Bean
