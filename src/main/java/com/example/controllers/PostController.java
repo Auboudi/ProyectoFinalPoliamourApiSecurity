12d7334a6465f3eb93a2016a39bacaa8ae1ebdca
aboudinot <aurore.boudinot174@gmail.com>
2023-04-12 18:32:49 +0200

    Añadido CrossOrigin en controllers

--- src/main/java/com/example/controllers/PostController.java
+++ src/main/java/com/example/controllers/PostController.java
@@ -20,6 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +45,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/posts")
+@CrossOrigin(origins = {"*"})
 
 public class PostController {
     // @Autowired
--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -21,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,6 +49,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = {"*"})
 
 public class UserController {
 
