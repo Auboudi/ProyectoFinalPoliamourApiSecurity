3c7ff19835aa4ed40b9fb19de1bb94d966be3d11
=ddelvill <dayanasdelvillar@gmail.com>
2023-04-12 20:08:09 +0200

    Commit Pre Merge Cristina

--- src/test/java/com/example/services/PostServiceTests.java
+++ src/test/java/com/example/services/PostServiceTests.java
@@ -2,8 +2,23 @@ package com.example.services;
 
 import static org.mockito.BDDMockito.given;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.dao.UserDao;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@SpringBootTest
 public class PostServiceTests {
+
+    private UserDao userDao;
+
+    
     
 }
