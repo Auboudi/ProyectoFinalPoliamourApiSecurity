c9d4eb8c0347a72db03bcf6c0617f982027610b4
aboudinot <aurore.boudinot174@gmail.com>
2023-04-13 12:14:42 +0200

    para captura

--- src/main/java/com/example/controllers/PostController.java
+++ src/main/java/com/example/controllers/PostController.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.modelmapper.ModelMapper;
+//import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
@@ -49,8 +49,8 @@ import jakarta.validation.Valid;
 @CrossOrigin(origins = {"*"})
 
 public class PostController {
-    @Autowired
-    private ModelMapper modelMapper;
+     //@Autowired
+    // private ModelMapper modelMapper;
     @Autowired
     private PostService postService;
 
